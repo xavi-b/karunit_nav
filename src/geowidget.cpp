17a2abb44c4f7a5b03b8025fa4b9b6192d7f3fb5
xavi-b <developer@xavi-b.fr>
2020-05-21 13:47:21 +0200

    qrc corrections

--- src/geowidget.cpp
+++ src/geowidget.cpp
@@ -4,7 +4,7 @@ GeoWidget::GeoWidget(QQmlEngine* engine, QWidget *parent)
     : QQuickWidget(engine, parent)
 {
     this->setResizeMode(QQuickWidget::SizeRootObjectToView);
-    this->setSource(QUrl("qrc:/qml/Main.qml"));
+    this->setSource(QUrl("qrc:/karunit_nav/qml/Main.qml"));
 
     connect(this->rootObject(), SIGNAL(call(QString)), this, SIGNAL(call(QString const&)));
 }
